66701869b76a51504a50f28c06fd27d5b548535f
nadeenghali <nadeenghali1@gmail.com>
2018-10-04 13:42:02 +0200

    Node class progress

--- Project1/src/Node.java
+++ Project1/src/Node.java
@@ -1,4 +1,26 @@
 
 public class Node {
-
+	Node parent;
+	State state;
+	JonSnowOperation previousOperator;
+	int depth;
+	int cost;
+	
+	// root constructor
+	public Node(){
+		this.parent = null;
+		this.previousOperator = null;
+		this.depth = 0;
+		this.cost = 0;
+		
+		//state initialization
+	}
+	
+	// node constructor
+	public Node(Node parent, JonSnowOperation previousOperator){
+		this.parent = parent;
+		this.depth = parent.depth + 1;
+		this.cost = parent.cost;
+		this.previousOperator = previousOperator;
+	}
 }
